62f675341eb9c212a493787d34f46fdf09f1564d
Eric SHIM <hyunwoo8504@gmail.com>
2025-07-24 08:48:04 +0900

    Feat: 투명(시뮬레이션) - 실패

--- 테스트/투명.py
+++ 테스트/투명.py
@@ -1,12 +1,10 @@
-n, m = map(int, input().split())
-xyxyList = [ list(map(int, input().split())) for i in range(n) ]
+paperNum, overlapPaper = map(int, input().split())
+xyxyMatrix = [ list(map(int, input().split())) for i in range(paperNum) ]
 
-matrixOrigin = [ [0] * 100 for i in range(100) ]
-for xyxyArr in xyxyList:
-    x1, y1 = int(xyxyArr[0]), int(xyxyArr[1])
-    x2, y2 = int(xyxyArr[2]), int(xyxyArr[3])
-    #distanceRow = int(xyxyArr[3]) - int(xyxyArr[1])
-    #distanceCol = int(xyxyArr[2]) - int(xyxyArr[0])
+matrixOrigin = [ [0] * 100 for _ in range(100) ]
+for xyxyList in xyxyMatrix:
+    x1, y1 = int(xyxyList[0]), int(xyxyList[1])
+    x2, y2 = int(xyxyList[2]), int(xyxyList[3])
     for row in range(y1, y2+1):
         for col in range(x1, x2+1):
             matrixOrigin[row][col] += 1
@@ -14,13 +12,13 @@ for xyxyArr in xyxyList:
 #print(matrixOrigin)
 #print(m)
 
-
 cnt = 0
 for i in range(100):
     for j in range(100):
-        if matrixOrigin[i][j] > m:
+        if matrixOrigin[i][j] > overlapPaper:
             cnt += 1
-if n == 0:
+
+if paperNum == 0:
     print(0)
 else:
     print(cnt)
