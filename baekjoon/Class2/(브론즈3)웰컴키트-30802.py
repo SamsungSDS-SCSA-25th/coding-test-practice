8553a9796729584938d02b82f3b12c654d6fd5dc
Eric SHIM <hyunwoo8504@gmail.com>
2025-07-23 15:02:42 +0900

    Feat: 소수찾기(단순구현)

--- baekjoon/Class2/(브론즈3)웰컴키트-30802.py
+++ baekjoon/Class2/(브론즈3)웰컴키트-30802.py
@@ -1,14 +1,16 @@
 n = int(input())
-sizeNumList = list(map(int, input().split()))
-t, p = map(int, input().split())
+numList = list(map(int, input().split()))
 
-tCnt, pCnt1, pCnt2 = 0, 0, 0
-for sizeNum in sizeNumList:
-    if sizeNum % t == 0:
-        tCnt += sizeNum // t
-    else:
-        tCnt += sizeNum // t + 1
-pCnt1 = n // p
-pCnt2 = n % p
-print(tCnt)
-print(pCnt1, pCnt2)
+primeNumberCnt = 0
+for num in numList:
+    flag = True
+    for idx in range(2, int(num**0.5)+1):
+        if num % idx == 0:
+            flag = False
+            break
+    if num == 1:
+        flag = False
+    if flag:
+        primeNumberCnt += 1
+
+print(primeNumberCnt)
