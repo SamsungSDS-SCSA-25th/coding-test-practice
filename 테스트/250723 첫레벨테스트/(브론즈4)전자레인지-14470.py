88ba33fc3d56d6347c677e67f4e93ae0c893e2a2
Eric SHIM <hyunwoo8504@gmail.com>
2025-07-24 10:29:14 +0900

    Refactor: 전자레인지-14470(함수모듈화)

--- 테스트/250723 첫레벨테스트/(브론즈4)전자레인지-14470.py
+++ 테스트/250723 첫레벨테스트/(브론즈4)전자레인지-14470.py
@@ -1,3 +1,4 @@
+'''
 meatTemperature = int(input())
 targetTemperature = int(input())
 upTemperatureIced = int(input())
@@ -33,5 +34,48 @@ while True:
         secCnt += unfreezeTemperatureIced
         secCnt += ( upTemperatureAir * (targetTemperature - meatTemperature))
         break
+
+print(secCnt)
+'''
+
+# 중복되는 기능 함수화???
+meatTemperature = int(input())
+targetTemperature = int(input())
+upTemperatureIced = int(input())
+unfreezeTemperatureIced = int(input())
+upTemperatureAir = int(input())
+
+def minusUp(temperature, sec):
+    temperature += 1
+    sec += upTemperatureIced
+    return temperature, sec
+
+def plusUp(temperature, sec):
+    temperature += 1
+    sec += upTemperatureAir
+    return temperature, sec
+
+def zeroUp(temperature, sec):
+    sec += unfreezeTemperatureIced
+    sec += upTemperatureAir # WARNING. 해동 이후에 1도로 올라가는 시간도 넣어야 함
+    temperature += 1
+    return temperature, sec
+
+secCnt = 0
+while True: # 0을 기준으로, 영하면 해동시간 고려, 영상이면 해동시간 고려 x, 온도가 0인 것은 없음
+
+    if targetTemperature <= meatTemperature: # 공통 내용은 제일 처음 validate
+        #print(meatTemperature, secCnt)
+        break
+
+    if meatTemperature > 0:
+        meatTemperature, secCnt = plusUp(meatTemperature, secCnt)
+
+    elif meatTemperature < 0:
+        meatTemperature, secCnt = minusUp(meatTemperature, secCnt)
+        #print(meatTemperature, secCnt)
+
+    elif meatTemperature == 0:
+        meatTemperature, secCnt = zeroUp(meatTemperature, secCnt)
 
 print(secCnt)
