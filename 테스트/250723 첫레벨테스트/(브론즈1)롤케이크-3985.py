f8640e72cfb5ddccd532e9abb0190f4d282a7ca9
Eric SHIM <hyunwoo8504@gmail.com>
2025-07-24 12:07:38 +0900

    Refactor: 롤케이크-3985(변수명 줄이기, .index() 클린코드)

--- 테스트/250723 첫레벨테스트/(브론즈1)롤케이크-3985.py
+++ 테스트/250723 첫레벨테스트/(브론즈1)롤케이크-3985.py
@@ -1,19 +1,20 @@
+'''
 cakeLength = int(input())
 people = int(input())
 sizeList = [ list(map(int, input().split())) for _ in range(people) ]
 
-cakeSituList = [0] * cakeLength
+cakeStatusList = [0] * cakeLength
 thinkEatCakeList = []
 realEatCakeList = [0] * people
 
-for personIdx, personSize in enumerate(sizeList):
+for personIdx, personSize in enumerate(sizeList): # 원소가 리스트 형태로 나오더라도 enumerate 사용가능
     start, end = personSize[0], personSize[1]
     thinkEatCakeList.append(end - start)
     #print(thinkEatCakeList)
     for cakeIdx in range(start, end+1):
-        if cakeSituList[cakeIdx - 1] == 0:
+        if cakeStatusList[cakeIdx - 1] == 0:
             realEatCakeList[personIdx] += 1
-            cakeSituList[cakeIdx - 1] += 1
+            cakeStatusList[cakeIdx - 1] += 1
 
 
 #print(cakeSituList) # ok
@@ -38,3 +39,31 @@ if realEatCakeList.count(maxRealEatCake) > 1:
             break
 else:
     print(realEatCakeList.index(maxRealEatCake) + 1) # ok
+'''
+
+# 변수명 길이를 줄여라 -> 예영 프로님 조언
+
+cakeLength = int(input())
+people = int(input())
+sizeList = [ list(map(int, input().split())) for _ in range(people) ]
+
+cakeList = [0] * cakeLength
+thinkList = []
+realList = [0] * people
+
+for personIdx, personSize in enumerate(sizeList): # 원소가 리스트 형태로 나오더라도 enumerate 사용가능
+
+    start, end = personSize[0], personSize[1]
+
+    thinkList.append(end - start)
+
+    for cakeIdx in range(start, end+1):
+        if cakeList[cakeIdx - 1] == 0:
+            realList[personIdx] += 1
+            cakeList[cakeIdx - 1] += 1
+
+# .index() 는 동일한 것이 있을 경우 맨 앞에 것을 출력
+maxThink = max(thinkList)
+maxReal = max(realList)
+print(thinkList.index(maxThink) + 1) # ok
+print(realList.index(maxReal) + 1) # ok
