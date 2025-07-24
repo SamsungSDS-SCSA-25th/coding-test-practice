39a5bf2058d223265f332e20ca92b363169b4fd2
Eric SHIM <hyunwoo8504@gmail.com>
2025-07-24 10:44:35 +0900

    Refactor: 슈퍼마리오-2851(불필요한 코드 정리)

--- 테스트/250723 첫레벨테스트/(브론즈1)슈퍼마리오-2851.py
+++ 테스트/250723 첫레벨테스트/(브론즈1)슈퍼마리오-2851.py
@@ -1,3 +1,4 @@
+'''
 mushroomsScoreList = [ int(input()) for _ in range(10) ]
 
 tempSum = 0
@@ -15,4 +16,27 @@ for idx, mushroomsScore in enumerate(mushroomsScoreList):
             break
 
 if tempSum < 100:
+    print(tempSum)
+'''
+
+mushroomsScoreList = [ int(input()) for _ in range(10) ]
+
+tempSum = 0
+for mushroomScore in mushroomsScoreList:
+
+    tempSum += mushroomScore
+
+    if tempSum == 100:
+        print(tempSum)
+        break
+
+    if tempSum > 100: # 반복문 수행중, 100이 넘어가면 앞 뒤 체크 (절댓값 이용)
+        if abs(tempSum - 100) > abs(tempSum - mushroomScore - 100):
+            print(tempSum - mushroomScore)
+            break
+        elif abs(tempSum - 100) <= abs(tempSum - mushroomScore - 100):
+            print(tempSum)
+            break
+
+if tempSum < 100: # 반복문 수행이후 100이 안남는 경우 리스트총합이 100과 가장 가까움
     print(tempSum)
