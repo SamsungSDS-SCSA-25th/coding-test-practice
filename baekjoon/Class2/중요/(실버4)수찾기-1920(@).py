45cc3c0e721a5806fda450bbfc8f1ad781aea782
Eric SHIM <hyunwoo8504@gmail.com>
2025-07-25 10:02:02 +0900

    Practice: 수찾기(이분탐색 연습)

--- baekjoon/Class2/중요/(실버4)수찾기-1920(@).py
+++ baekjoon/Class2/중요/(실버4)수찾기-1920(@).py
@@ -1,12 +1,12 @@
 # for문 2번 돌리면 2^10이라 뻑날거 같음 -> 1초 2^8
 # 이분탐색 -> 오름차순, 반복적으로 반으로 나눠서(mid) (target)검색 O(logN)
 
-def binary_search(target, data):
+def binarySearch(target, data):
     start = 0
     end = len(data)-1
 
-    while start <= end: #
-        mid = (start+end)//2
+    while start <= end:
+        mid = (start+end) // 2
         if data[mid] == target:
             return mid
         elif data[mid] < target:
@@ -16,6 +16,7 @@ def binary_search(target, data):
 
     return
 
+
 n = int(input())
 nList = list(map(int, input().split()))
 m = int(input())
@@ -23,7 +24,7 @@ mList = list(map(int, input().split()))
 
 nList.sort() # 이분 탐색마다 sort하는 것은 시간낭비
 for m in mList: # nList 중 m이 있나?
-    if binary_search(m, nList) is not None: # 어떤 정수가 반환될지 몰라 None으로 함. flag 세우는 방법도 있을듯
+    if binarySearch(m, nList) is not None: # 어떤 정수가 반환될지 몰라 None으로 함. flag 세우는 방법도 있을듯
         print(1)
-    elif binary_search(m, nList) is None:
+    elif binarySearch(m, nList) is None:
         print(0)
