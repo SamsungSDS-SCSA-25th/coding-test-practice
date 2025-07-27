39560b2816a821a2f294c3037060ca4b0d5a4a00
Eric SHIM <hyunwoo8504@gmail.com>
2025-07-28 08:14:24 +0900

    Repeat(2): 원재의메모리복구하기 - 1차원, 정렬

--- 테스트/250725 팀편성테스트/(1)원재의메모리복구하기-정렬/원재의메모리복구하기.py
+++ 테스트/250725 팀편성테스트/(1)원재의메모리복구하기-정렬/원재의메모리복구하기.py
@@ -1,3 +1,4 @@
+# XO
 # 처음에 무슨 소리인가 당황...
 ## 집에 와서 다시 읽어보니 바로 이해되네... -> 너무 빠르게 읽으려고 하지말자
 ## 아이디어는 직접 써봐야 나온다.
@@ -5,22 +6,18 @@
 t = int(input())
 
 for index in range(t):
-    numList = list(map(int, list(input())))
-    emptyList = [0] * len(numList)
-    cnt = 0
-    for idx, num in enumerate(numList):
-        if num == emptyList[idx]:
+    originMemory = list(map(int, list(input())))
+    zeroMemory = [ 0 for _ in range(len(originMemory)) ]
+    # 00000 -> originMemory
+    # print(originMemory)
+    changeCnt = 0
+    for idx, digit in enumerate(originMemory):
+        if digit == zeroMemory[idx]:
             continue
+        elif digit != zeroMemory[idx]:
+            changeCnt += 1
+            for tempIdx in range(idx, len(originMemory)):
+                zeroMemory[tempIdx] = digit
 
-        if emptyList[idx] == 0:
-            for i in range(idx, len(numList)):
-                emptyList[i] = 1
-        elif emptyList[idx] == 1:
-            for i in range(idx, len(numList)):
-                emptyList[i] = 0
-
-        #print(emptyList)
-
-        cnt += 1
-
-    print(f'#{index+1} {cnt}')
+    # print(changeCnt)
+    print(f'#{index+1} {changeCnt}')
