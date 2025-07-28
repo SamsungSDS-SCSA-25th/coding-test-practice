10a86e1cc850691039a19897e93f0239271bddd8
Eric SHIM <hyunwoo8504@gmail.com>
2025-07-28 09:03:04 +0900

    Repeat(2): 정사각형판정 - 2차원, 시뮬레이션

--- 테스트/250725 팀편성테스트/(4)정사각형판정@@-2차원, 시뮬레이션/정사각형판정.py
+++ 테스트/250725 팀편성테스트/(4)정사각형판정@@-2차원, 시뮬레이션/정사각형판정.py
@@ -1,8 +1,11 @@
+# XX
+
 # 문제 제대로 읽자.. 하나의 정사각형+1x1도 가능!! -> 여러개의 정사각형이 아님(deque 써야할듯)
 # '#'이 들어있는 좌표를 탐색, 저장
 # 바운딩박스를 만들어 -> 최소, 최대 좌표 4개 만들어서 그 안에서 #으로 가득차있는지 확인
 # x, y -> col, row 로 저장 유의
 
+'''
 t = int(input())
 
 for index in range(t):
@@ -37,4 +40,43 @@ for index in range(t):
     if flag:
         print(f'#{index + 1} yes')
     elif not flag:
+        print(f'#{index + 1} no')
+'''
+
+# ""하나""의 정사각형이 있는 판단하는 알고리즘
+# 바운딩박스 만들기
+# -> 정사각형인지 체크하는 조건을 빼먹어서 또 틀림... 조건을 정리하는 습관 필요함
+
+t = int(input())
+
+for index in range(t):
+    n = int(input())
+    matrix = [ list(input()) for _ in range(n) ]
+    # print(matrix)
+    minX, minY, maxX, maxY = float('inf'), float('inf'), float('-inf'), float('-inf')
+    for row in range(n):
+        for col in range(n):
+            if matrix[row][col] == '#':
+                minX = min(minX, col)
+                minY = min(minY, row)
+                maxX = max(maxX, col)
+                maxY = max(maxY, row)
+
+    # print(f'{minX=}, {minY=}, {maxX=}, {maxY=}')
+
+    shapCnt, flag = 0, True
+    for row in range(minY, maxY + 1):
+        for col in range(minX, maxX + 1):
+            if matrix[row][col] == '#':
+                shapCnt += 1
+            elif matrix[row][col] != '#':
+                flag = False
+                break
+
+        if not flag:
+            break
+
+    if shapCnt == ((maxY - minY + 1) * (maxX - minX + 1)) and (maxY - minY + 1) == (maxX - minX + 1): # 정사각형인지도 포함
+        print(f'#{index + 1} yes')
+    else:
         print(f'#{index + 1} no')
