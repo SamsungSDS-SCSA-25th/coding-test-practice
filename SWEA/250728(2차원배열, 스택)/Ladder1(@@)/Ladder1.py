ce1b1188b24b83da437a9f3b999c5d753b9d1a36
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-01 08:48:13 +0900

    Feat(2): Ladder1 (2차원,시뮬레이션)

--- SWEA/250728(2차원배열, 스택)/Ladder1(@@)/Ladder1.py
+++ SWEA/250728(2차원배열, 스택)/Ladder1(@@)/Ladder1.py
@@ -1,3 +1,4 @@
+## XX
 # 맨 처음 행에 1인 인덱스 찾아서 저장
 # 좌우에 1이 있으면 이동한다 그곳으로 이동
 # 1. 내려가면서 좌우 1 체크 (0,1) -> (-1,0) & (1,0)
@@ -66,19 +67,6 @@ for index in range(t):
     print(f'#{index+1} {twoAnswer}')
 '''
 #2 -> 아래에서 위로 올라가는 것으로 구하기 (시간복잡도를 줄일 수 있음)
-# 사용자정의함수를 정의를 해보자
-
-def up(curCol, curRow):
-    global ladderMatrix
-    return
-
-def left(curCol, curRow):
-    global ladderMatrix
-    return
-
-def right(curCol, curRow):
-    global ladderMatrix
-    return
 
 t = int(input())
 for index in range(t):
@@ -89,16 +77,48 @@ for index in range(t):
         if ladderMatrix[endRow][col] == 2:
             endCol = col
 
+    nxtCol, nxtRow = endCol, endRow-1
     switchIdx = 0 # 0: up / 1: left / 2: right
     while True:
-        if row == 0 and ladderMatrix[row][col] == 1:
-            startRow, startCol = row, col
-            break
 
-        if switchIdx == 0:
+        if switchIdx == 0: # 위로 올라가는 경우
+            upCol, upRow = nxtCol, nxtRow-1
+            leftCol, leftRow, rightCol, rightRow = nxtCol-1, nxtRow, nxtCol+1, nxtRow
+            if nxtRow == 0: # 시작지점에 도착
+                startCol, startRow = nxtCol, 0
+                break
 
+            if 0<=leftCol<100 and ladderMatrix[leftRow][leftCol] == 1: # left가 1이면
+                nxtCol, nxtRow = leftCol, leftRow
+                switchIdx = 1
+                continue
 
+            if 0<=rightCol<100 and ladderMatrix[rightRow][rightCol] == 1: # right가 1이면
+                nxtCol, nxtRow = rightCol, rightRow
+                switchIdx = 2
+                continue
 
+            nxtCol, nxtRow = upCol, upRow
+            ### 위 조건 중 아무것도 해당하지 않으면, 계속 위로 올라감
 
+        elif switchIdx == 1: # 벽이거나, 0을 leftCol에서 만나는 경우 다시 위로 올라가야함
+            leftCol, leftRow = nxtCol-1, nxtRow
+            if leftCol<0 or ladderMatrix[leftRow][leftCol] == 0: # 위로 올라가야하는 경우
+                nxtCol, nxtRow = nxtCol, nxtRow-1
+                switchIdx = 0
+                continue
 
-    print(f'#{index+1} ')
+            nxtCol, nxtRow = leftCol, leftRow
+            ### 위 조건 중 아무것도 해당하지 않으면, 계속 왼쪽이동
+
+        elif switchIdx == 2: # 벽이거나, 0을 rightCol에서 만나는 경우 다시 위로 올라가야함
+            rightCol, rightRow = nxtCol + 1, nxtRow
+            if rightCol>99 or ladderMatrix[rightRow][rightCol] == 0:  # 위로 올라가야하는 경우
+                nxtCol, nxtRow = nxtCol, nxtRow - 1
+                switchIdx = 0
+                continue
+
+            nxtCol, nxtRow = rightCol, rightRow
+            ### 위 조건 중 아무것도 해당하지 않으면, 계속 왼쪽이동
+
+    print(f'#{index+1} {startCol}')
