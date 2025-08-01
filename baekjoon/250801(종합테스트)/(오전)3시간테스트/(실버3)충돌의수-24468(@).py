14cc21bb331d41f2123aaccf23a8dfffcbe1335d
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-08-01 23:39:06 +0900

    Feat: 충돌의수 (시뮬레이션,구현)

--- baekjoon/250801(종합테스트)/(오전)3시간테스트/(실버3)충돌의수-24468(@).py
+++ baekjoon/250801(종합테스트)/(오전)3시간테스트/(실버3)충돌의수-24468(@).py
@@ -2,7 +2,6 @@
 # 인덱스가 같아지는 것이 있으면 충돌횟수 기록하기
 ### (D) 충돌하더라도 그냥 공의 방향은 나둬도 됨 (예영프로님 아이디어)
 # 범위를 넘어서도 RL 바꾸기 -> 0~L
-from collections import deque
 
 L, N, T = map(int, input().split())
 ballInfoList = [ tuple(input().split()) for _ in range(N) ]
@@ -14,26 +13,35 @@ for ballInfo in ballInfoList:
         rightList.append(int(ballInfo[0]))
 
 # 첫 데이터 전처리
-leftList.sort() # 오름차순으로 정렬
-rightList.sort(key=lambda x: -x) # 내림차순으로 정렬
+leftList.sort() # 오름차순으로 정렬 1 3 4 5
+rightList.sort(key=lambda x: -x) # 내림차순으로 정렬 5 4 3 1
 
 # print(leftList)
 # print(rightList)
 
 boomCnt = 0
-for time in range(T): # 시간동안
-    for idx, left in enumerate(leftList):
-        leftList[idx] -= 1
-        if leftList[idx] == 0: # (D) 1이 아니라 0
-            rightList.append(leftList[idx])
-            leftList.pop(0)
-    for idx, right in enumerate(rightList):
-        rightList[idx] += 1
-        if rightList[idx] == L:
-            leftList.append(rightList[idx])
-            rightList.pop(0)
+for _ in range(T): # 시간동안
+    nxtLeftList, nxtRightList = [], [] # pop을 하지 않기 위해 사용 -> pop하면 index가 꼬임
 
-    tempSet = set(leftList) & set(rightList)
+    for left in leftList:
+        nxtLeft = left - 1
+        if nxtLeft == 0:
+            nxtRightList.append(nxtLeft)
+        else:
+            nxtLeftList.append(nxtLeft)
+
+    for right in rightList:
+        nxtRight = right + 1
+        if nxtRight == L:
+            nxtLeftList.append(nxtRight)
+        else:
+            nxtRightList.append(nxtRight)
+
+    # 충돌 판단
+    tempSet = set(nxtLeftList) & set(nxtRightList)
     boomCnt += len(tempSet)
+
+    # 갱신
+    leftList, rightList = nxtLeftList, nxtRightList
 
 print(boomCnt)
