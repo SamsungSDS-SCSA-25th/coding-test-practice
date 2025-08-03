9341267348f1b2c9731a6d155d0a5595df904f30
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-08-03 22:36:23 +0900

    Refactor: 김밥21개 (시뮬레이션) -> 함수사용하기

--- 주말스터디(코테)/1주차(250727)/타팀추천/(실버3)김밥21개-3254.py
+++ 주말스터디(코테)/1주차(250727)/타팀추천/(실버3)김밥21개-3254.py
@@ -7,6 +7,32 @@ directions = [(1,0),(0,1),(1,1),(1,-1)]
 
 matrix = [ [0]*7 for _ in range(6) ]
 
+def drop(matrix, playerCol, player):
+    for row in range(6):
+        if matrix[row][playerCol] == 0:
+            matrix[row][playerCol] = player
+            break
+
+def check4(matrix, player):
+    flag = False
+    for row in range(6):
+        for col in range(7):
+            if matrix[row][col] == player:
+                for dx, dy in directions:
+                    length = 0
+                    for dist in range(4):
+                        nxtCol, nxtRow = col + dx*dist, row + dy*dist
+                        if 0<=nxtCol<7 and 0<=nxtRow<6 and matrix[nxtRow][nxtCol] == player:
+                            length += 1
+                        else:
+                            break
+
+                    if length == 4:
+                        flag = True
+                        break
+
+    return flag
+
 # 상근이 김밥: 1 / 정인이 김밥: 2
 flag = False
 if not flag:
@@ -15,55 +41,21 @@ if not flag:
         sCol -= 1 # 열의 번호는 1번부터 시작함
         jCol -= 1
 
-        # 쌓기
-        for row in range(6):
-            if matrix[row][sCol] == 0:
-                matrix[row][sCol] = 1
-                break
+        # 쌓기 -> matrix는 iterable 해서 함수로 들어가도 값 변경됨
+        drop(matrix, sCol, 1)
+
         # 확인
-        for row in range(6):
-            for col in range(7):
-                if matrix[row][col] == 1:
-                    for dx, dy in directions:
-                        length = 0
-                        for dist in range(4):
-                            # print(f'{length=}')
-                            nxtRow, nxtCol = row + dy*dist, col + dx*dist
-                            if 0<=nxtRow<6 and 0<=nxtCol<7 and matrix[nxtRow][nxtCol] == 1:
-                                length += 1
-                            else:
-                                break
-                        if length == 4:
-                            flag = True
-                            answer = 'sk ' + str(ansIdx+1)
-                            break
+        flag = check4(matrix, 1)
 
         if flag:
+            answer = 'sk ' + str(ansIdx+1)
             break
 
-        # 쌓기
-        for row in range(6):
-            if matrix[row][jCol] == 0:
-                matrix[row][jCol] = 2
-                break
-        # 확인
-        for row in range(6):
-            for col in range(7):
-                if matrix[row][col] == 2:
-                    for dx, dy in directions:
-                        length = 0
-                        for dist in range(4):
-                            nxtRow, nxtCol = row + dy*dist, col + dx*dist
-                            if 0 <= nxtRow < 6 and 0 <= nxtCol < 7 and matrix[nxtRow][nxtCol] == 2:
-                                length += 1
-                            else:
-                                break
-                        if length == 4:
-                            flag = True
-                            answer = 'ji ' + str(ansIdx+1)
-                            break
+        drop(matrix, jCol, 2)
+        flag = check4(matrix, 2)
 
         if flag:
+            answer = 'ji ' + str(ansIdx + 1)
             break
 
         # print(matrix)
