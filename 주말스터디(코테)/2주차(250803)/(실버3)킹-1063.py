f135eaf74d59ed7614adcec6f15c314faba5c765
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-08-03 19:51:42 +0900

    Feat: 킹 (2차원,시뮬레이션)

--- 주말스터디(코테)/2주차(250803)/(실버3)킹-1063.py
+++ 주말스터디(코테)/2주차(250803)/(실버3)킹-1063.py
@@ -1,6 +1,6 @@
 # 돌이 있는 경우의 분기처리
 # 알파벳을 인덱스 형태로 접근하기
-
+''' #1
 colList = ['A','B','C','D','E','F','G','H']
 DIRECTIONS = {
     'R': (1,0), 'L': (-1,0), 'T':(0,1), 'B':(0,-1),
@@ -34,6 +34,44 @@ for move in moveList:
     kingRow += dy
 
     # print(f'{kingCol=} {kingRow=} {stoneCol=} {stoneRow=}')
+
+print(colList[kingCol]+str(kingRow+1))
+print(colList[stoneCol]+str(stoneRow+1))
+'''
+#2
+king, stone, n = input().split()
+moveList = [ input() for _ in range(int(n)) ]
+
+directions = {
+    'R': (1,0),
+    'L': (-1,0),
+    'B': (0,-1),
+    'T': (0,1),
+    'RT': (1,1),
+    'LT': (-1,1),
+    'RB': (1,-1),
+    'LB': (-1,-1)
+}
+colList = list('ABCDEFGH')
+
+kingCol, kingRow =  colList.index(list(str(king))[0]), int(list(str(king))[1])-1
+stoneCol, stoneRow = colList.index(list(str(stone))[0]), int(list(str(stone))[1])-1
+
+for move in moveList:
+    dx, dy = directions[move][0], directions[move][1]
+
+    if not (0<=kingCol + dx<8 and 0<=kingRow + dy<8): # king 범위초과
+        continue
+
+    if kingCol + dx == stoneCol and kingRow + dy == stoneRow:
+        if 0<=stoneCol + dx<8 and 0<=stoneRow + dy<8:
+            stoneCol += dx
+            stoneRow += dy
+        else:
+            continue
+
+    kingCol += dx
+    kingRow += dy
 
 print(colList[kingCol]+str(kingRow+1))
 print(colList[stoneCol]+str(stoneRow+1))
