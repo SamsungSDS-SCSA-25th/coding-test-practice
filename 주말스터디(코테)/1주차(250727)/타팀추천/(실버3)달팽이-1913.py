5ddbc24813a6ce751af947183e261cf05bf08c57
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-04 09:24:59 +0900

    Feat: 달팽이 (구현)

--- 주말스터디(코테)/1주차(250727)/타팀추천/(실버3)달팽이-1913.py
+++ 주말스터디(코테)/1주차(250727)/타팀추천/(실버3)달팽이-1913.py
@@ -5,20 +5,48 @@
 n = int(input())
 target = int(input())
 
-directions = [(1,0), (1,0), (0,-1), (-1,0)]
+directions = [(0,1), (1,0), (0,-1), (-1,0)]
 matrix = [ [0]*n for _ in range(n) ]
-startCol, startRow = n // 2, n // 2
-matrix[startRow][startCol] = 1
 
-for num in range(n*n + 1):
-    num += 1
+num = 1
+twoCnt, progressCnt, directionIdx = 2, 1, 0
+while True:
+    if num == n*n:
+        break
 
+    # 제일 처음은 따로
     if num == 1:
         curCol, curRow = n // 2, n // 2
         matrix[curRow][curCol] = num
 
-    for dx, dy in directions:
-        nxtCol, nxtRow =
+        num += 1
+        continue
 
+    if twoCnt == 0:
+        progressCnt += 1
+        twoCnt = 2
 
-    curCol, curRow = nxtCol, nxtRow
+    twoCnt -= 1
+    direction = directions[directionIdx % 4]
+
+    for dist in range(1, progressCnt+1):
+        nxtCol, nxtRow = curCol + direction[0], curRow + direction[1]
+        matrix[nxtRow][nxtCol] = num
+
+        if num == n * n:
+            break
+
+        num += 1
+        curCol, curRow = nxtCol, nxtRow
+
+    directionIdx += 1
+
+for row in range(n):
+    for col in range(n):
+        if matrix[::-1][row][col] == target:
+            ansCol, ansRow = col, row
+
+        print(matrix[::-1][row][col], end=' ')
+    print()
+
+print(ansRow+1, ansCol+1)
