d4b9a2f600ad5f68858ffd979a7f77b984c33f5a
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-04 16:25:43 +0900

    Refactor: 완전이진트리 노드의합 (재귀) -> 루프말고 재귀로 풀이

--- SWEA/2주차/250804(재귀)/노드의합/노드의합.py
+++ SWEA/2주차/250804(재귀)/노드의합/노드의합.py
@@ -1,5 +1,6 @@
 # 완전 이진트리
 
+''' # 루프로 풀기
 t = int(input())
 for i in range(1, t+1):
     n, m, l = map(int, input().split())
@@ -18,3 +19,24 @@ for i in range(1, t+1):
         tree[idx] = tree[left] + (tree[right] if right <= n else 0)
 
     print(f'#{i} {tree[l]}')
+'''
+# 재귀로 풀기
+def postOrder(node):
+    if not (1 <= node <= n):
+        return 0
+
+    leftSum = postOrder(node*2)
+    rightSum = postOrder(node*2 + 1)
+    return leftSum + rightSum + tree[node]
+
+t = int(input())
+for i in range(1, t+1):
+    n, m, l = map(int, input().split())
+    leafList = [ tuple(map(int, input().split())) for _ in range(m) ]
+
+    tree = [0] * (n+1)
+    for leafNode, num in leafList:
+        tree[leafNode] = num
+
+    ans = postOrder(l)
+    print(f'#{i} {ans}')
