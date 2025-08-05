29421ed2ea82364659b42b32e20ed507f84d41e2
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-05 11:10:14 +0900

    Refactor: 주사위던지기2 (백트래킹 기초) -> 중복제거하는 아이디어

--- SWEA/2주차/250805(백트래킹)/주사위던지기2/주사위던지기2.py
+++ SWEA/2주차/250805(백트래킹)/주사위던지기2/주사위던지기2.py
@@ -1,3 +1,4 @@
+''' # 정렬로 중복 줄이기
 def dfs(n, ans):
     global lst
     #1 종료조건
@@ -12,6 +13,24 @@ def dfs(n, ans):
     for rollIdx in range(1, 7): # 주사위 1~6까지 굴리기
         # print(f'#{rollIdx}')
         dfs(n+1, ans + [rollIdx])
+'''
+# 아이디어로 풀이
+def dfs(n, ans):
+    global lst
+    #1 종료조건
+    if n == targetN:
+        print(*ans)
+        return
+
+    #2 하부함수 호출(재귀)
+    if n == 0: # 처음
+        for rollIdx in range(1, 7):
+            # print(f'#{rollIdx}')
+            dfs(n+1, ans + [rollIdx])
+    else: # 그 이후
+        for rollIdx in range(ans[-1], 7): # 전 노드의 숫자부터 주사위 굴리기 시작하면 됨
+            # print(f'#{rollIdx}')
+            dfs(n+1, ans + [rollIdx])
 
 #############################
 
@@ -22,4 +41,3 @@ for i in range(1, tc+1):
     print(f'#{i}')
     lst =[]
     dfs(0, [])
-    print()
