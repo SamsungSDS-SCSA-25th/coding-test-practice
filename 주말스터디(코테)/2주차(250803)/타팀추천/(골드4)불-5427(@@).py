4ce171f66f13f6dc8101bc1a003b7b32733f0aef
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-05 17:35:06 +0900

    Refactor: 불 (BFS응용,시뮬레이션) -> 방문행렬없이 matrix를 직접 바꾸면서 가는 방법 -> BFS 한번만 해도됨 (불->상근이순 / 재귀를 하지않으므로) | 메모리초과는 q가 터진 것((D)@에서 .만 방문하게 했어야함)

--- 주말스터디(코테)/2주차(250803)/타팀추천/(골드4)불-5427(@@).py
+++ 주말스터디(코테)/2주차(250803)/타팀추천/(골드4)불-5427(@@).py
@@ -1,3 +1,4 @@
+'''
 # 범위 밖으로 언제 나가는지 구하는 문제
 # *좌표에서는 4방향으로 매초마다 번진다. -> 따로함수 구현
 # 벽은 통과하지 못한다.
@@ -33,7 +34,7 @@ def bfs(startCol, startRow):
     global matrix, burnMatrix, visited, n, m
     q = deque([(startCol, startRow, 0)])
     visited[startRow][startCol] = True
-    
+
     while q:
         curCol, curRow, curSec = q.popleft()
         for dx, dy in DIRECTIONS:
@@ -44,7 +45,7 @@ def bfs(startCol, startRow):
             if matrix[nxtRow][nxtCol] != '.' or visited[nxtRow][nxtCol]: # 벽에 부딪히거나, 이미 방문
                 continue
 
-            if burnMatrix[nxtRow][nxtCol] <= nxtSec: # 불과 만남
+            if burnMatrix[nxtRow][nxtCol] <= nxtSec: #### 불과 만남
                 continue
 
             visited[nxtRow][nxtCol] = True
@@ -75,7 +76,6 @@ for _ in range(t):
                 visited[row][col] = True
             elif matrix[row][col] == '#':  # 방문 안할곳은 방문처리
                 burnVisited[row][col] = True
-                burnVisited[row][col] = True
                 visited[row][col] = True
             elif matrix[row][col] == '@':
                 startCol, startRow = col, row
@@ -89,3 +89,71 @@ for _ in range(t):
     answer = bfs(startCol, startRow)
 
     print(answer)
+'''
+# BFS의 특징을 활용해서 한번에 푼다
+# 1. *에서 1초 지났을때 큐에서 빼고 넣기, 2. 상근이가 이동할 수 있는 곳으로 가기 -> 재귀하지 않아 같이해도 괜찮다
+# (col, row, sec, state)로 저장
+# 배열 실제로 바꾸기
+### q가 터져서 "메모리초과" -> 방문을 어떻게 할지 조건나누기 잘하자
+
+from collections import deque
+DIRECTIONS = (
+    (0, 1),
+    (1, 0),
+    (0, -1),
+    (-1, 0),
+)
+
+# bfs 한번에 하기
+def bfs(startCol, startRow):
+    global visited, burnList
+    q = deque([])
+    ### q에 넣는 순서 중요!!!
+    #1 불
+    for col, row in burnList:
+        q.append((col, row, 0, '*'))
+
+    #2 상근이
+    q.append((startCol, startRow, 0, '@'))
+    # visited[startRow][startCol] = True
+
+    while q:
+        curCol, curRow, curSec, curState = q.popleft()
+        # print(f'{curCol},{curRow},{curSec},{curState}')
+
+        for dx, dy in DIRECTIONS:
+            nxtCol, nxtRow, nxtSec = curCol + dx, curRow + dy, curSec + 1
+            # print(matrix)
+            if curState == '@' and not (0 <= nxtCol < m and 0 <= nxtRow < n):
+                return nxtSec
+
+            if 0<=nxtCol<m and 0<=nxtRow<n:
+                if curState == '@':
+                    if matrix[nxtRow][nxtCol] == '.': # (D) @은 또 방문하지 않는다 !!!
+                    # if matrix[nxtRow][nxtCol] == '.' and not visited[nxtRow][nxtCol]:
+                        q.append((nxtCol, nxtRow, nxtSec, '@'))
+                        # visited[nxtRow][nxtCol] = True
+                        matrix[nxtRow][nxtCol] = '@'
+
+                elif curState == '*' and matrix[nxtRow][nxtCol] in '.@':
+                    q.append((nxtCol, nxtRow, nxtSec, '*'))
+                    matrix[nxtRow][nxtCol] = '*'
+
+    return 'IMPOSSIBLE'
+
+
+tc = int(input())
+for i in range(1, tc+1):
+    m, n = map(int, input().split())
+    matrix = [ list(input()) for _ in range(n) ]
+
+    # visited = [ [False]*m for _ in range(n) ]
+    burnList = []
+    for row in range(n):
+        for col in range(m):
+            if matrix[row][col] == '@':
+                startCol, startRow = col, row
+            elif matrix[row][col] == '*':
+                burnList.append((col, row))
+
+    print(bfs(startCol, startRow))
