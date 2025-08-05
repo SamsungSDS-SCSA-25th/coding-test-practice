f01f3bc8e9d43a0c7f99ccad3953952f18356958
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-05 14:12:08 +0900

    Feat: 불 (BFS 응용,시뮬레이션)

--- 주말스터디(코테)/2주차(250803)/타팀추천/(골드4)불-5427(@@).py
+++ 주말스터디(코테)/2주차(250803)/타팀추천/(골드4)불-5427(@@).py
@@ -3,59 +3,89 @@
 # 벽은 통과하지 못한다.
 
 from collections import deque
+DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
+INF = 10**8
 
 # bfs 이므로 visited를 여러개 만들필요 없다 -> 재귀하지 않는다.
-def burn(matrix, visited):
-    burnList = []
-    for row in range(n):
-        for col in range(m):
-            if matrix[row][col] == '*': # (D) 초기 좌표를 저장하고 불을 넣어야함...
-                burnList.append((col, row))
+# (D) 좌표별로 burn까지 걸리는 시간을 저장한다. -> 계속 burn을 호출하지 않기 위함
+def burnBFS(startBurnList): # -> BFS로 구현해야 할듯
+    global burnVisited, burnMatrix, n, m
 
-    for col, row in burnList:
+    q = deque()
+    for startCol, startRow, startSec in startBurnList:
+        q.append((startCol, startRow, startSec))
+        burnMatrix[startRow][startCol] = True
+        burnVisited[startRow][startCol] = True
+
+    # * 확산시키면서, *좌표 저장
+    while q:
+        curCol, curRow, curSec = q.popleft()
         for dx, dy in DIRECTIONS:
-            nxtCol, nxtRow = col + dx, row + dy
-            if 0<=nxtCol<m and 0<=nxtRow<n and not visited[nxtRow][nxtCol]:
-                visited[nxtRow][nxtCol] = True
-                matrix[nxtRow][nxtCol] = '*'
+            nxtCol, nxtRow, nxtSec = curCol + dx, curRow + dy, curSec + 1
+            if 0<=nxtCol<m and 0<=nxtRow<n and not burnVisited[nxtRow][nxtCol] and matrix[nxtRow][nxtCol] == '.':
+                q.append((nxtCol, nxtRow, nxtSec))
+                burnMatrix[nxtRow][nxtCol] = nxtSec
+                burnVisited[nxtRow][nxtCol] = True
 
-    return
+    return burnMatrix
 
-
-DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
-def bfs(startCol, startRow, visited, matrix):
+def bfs(startCol, startRow):
+    global matrix, burnMatrix, visited, n, m
     q = deque([(startCol, startRow, 0)])
     visited[startRow][startCol] = True
-    distList = []
     
     while q:
-        curCol, curRow, curDist = q.popleft()
+        curCol, curRow, curSec = q.popleft()
         for dx, dy in DIRECTIONS:
-            nxtCol, nxtRow, nxtDist = curCol + dx, curRow + dy, curDist + 1
+            nxtCol, nxtRow, nxtSec = curCol + dx, curRow + dy, curSec + 1
             if not (0<=nxtCol<m and 0<=nxtRow<n): # 탈출하는 경우
-                return nxtDist
+                return nxtSec
 
-            if nxtDist not in distList:
-                distList.append(nxtDist)
-                burn(matrix, visited)
+            if matrix[nxtRow][nxtCol] != '.' or visited[nxtRow][nxtCol]: # 벽에 부딪히거나, 이미 방문
+                continue
 
-            if 0<=nxtCol<m and 0<=nxtRow<n and matrix[nxtRow][nxtCol] == '.' and not visited[nxtRow][nxtCol]:
-                q.append((nxtCol, nxtRow, nxtDist))
-                visited[nxtRow][nxtCol] = True
+            if burnMatrix[nxtRow][nxtCol] <= nxtSec: # 불과 만남
+                continue
+
+            visited[nxtRow][nxtCol] = True
+            q.append((nxtCol, nxtRow, nxtSec))
 
     return 'IMPOSSIBLE'
+
+########################################
 
 t = int(input())
 for _ in range(t):
     m, n = map(int, input().split())
     matrix = [ list(input()) for _ in range(n) ]
 
-    visited = [[False] * m for _ in range(n)]
+    ### 초기 * 찾고, burnTime이 들어간 좌표 구하기
+    burnMatrix = [[INF] * m for _ in range(n)] # burnTime 있는 좌표
+
+    burnVisited = [[False] * m for _ in range(n)] # *용
+    visited = [[False] * m for _ in range(n)] # 일반
+
+    #1 * 위치 찾기 + . 제외하고 방문처리
+    startBurnList = []
     for row in range(n):
         for col in range(m):
-            if matrix[row][col] == '@':
-                startCol, startRow = col, row
-            if matrix[row][col] == '#' or matrix[row][col] == '*':
+            if matrix[row][col] == '*':  # (D) 초기 좌표를 저장하고 불을 넣어야함...
+                startBurnList.append((col, row, 0))
+                burnVisited[row][col] = True
                 visited[row][col] = True
-        
-    print(bfs(startCol, startRow, visited, matrix))
+            elif matrix[row][col] == '#':  # 방문 안할곳은 방문처리
+                burnVisited[row][col] = True
+                burnVisited[row][col] = True
+                visited[row][col] = True
+            elif matrix[row][col] == '@':
+                startCol, startRow = col, row
+                burnVisited[row][col] = True
+                visited[row][col] = True
+
+    #2 * 확산시키면서 최종 burnTime과 좌표 구하기
+    burnList = burnBFS(startBurnList)
+
+    #3 밖까지 최단거리 탐색 (제약: burnTime보다 좌표의 sec이 작아야 함)
+    answer = bfs(startCol, startRow)
+
+    print(answer)
