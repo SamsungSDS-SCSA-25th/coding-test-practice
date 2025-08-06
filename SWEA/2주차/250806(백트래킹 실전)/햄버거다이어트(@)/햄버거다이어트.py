7c9246c3465b4094918f8b9938ae794df6f65af4
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-06 10:55:32 +0900

    Refactor: 햄버거다이어트 (백트래킹) -> 조합에서 부분집합으로, 값의 갱신은 종료조건에서 할 수 있도록 하자!

--- SWEA/2주차/250806(백트래킹 실전)/햄버거다이어트(@)/햄버거다이어트.py
+++ SWEA/2주차/250806(백트래킹 실전)/햄버거다이어트(@)/햄버거다이어트.py
@@ -1,23 +1,23 @@
 # 민기가 좋아하는 햄버거의 최적의 조합
 # 칼로리 상한 아래에서, 가장 점수가 높은 햄버거 찾기
 # 조합 -> 백트래킹
+### 조합 말고,,, 부분집합으로 풀자...
 
-def dfs(curCalSum, curScoreSum, startIdx):
+def dfs(curCalSum, curScoreSum, curIdx):
     global scoreCalList, N, minCal, maxScore
     # 가지치기
     if curCalSum >= minCal:
         return
 
-    maxScore = max(maxScore, curScoreSum)  # N개가 꼭 아니어도 되므로, 계속 maxScore 갱신
-
     # 종료조건
-    if startIdx == N: # 조합은 idx가 5(범위초과)를 도달하면 끝!
+    if curIdx == N: # idx가 5(범위초과)를 도달하면 끝!
+        maxScore = max(maxScore, curScoreSum)
         # print(f'{maxScore=}')
         return
 
-    # 하부재귀
-    for idx in range(startIdx, N): # 조합에 대한 알고리즘
-        dfs(curCalSum + scoreCalList[idx][1], curScoreSum + scoreCalList[idx][0], idx + 1)
+    # 하부재귀 -> 부분집합 알고리즘 (암기!)
+    dfs(curCalSum, curScoreSum, curIdx + 1) # curIdx에서 갱신없음 (안고름)
+    dfs(curCalSum + scoreCalList[curIdx][1], curScoreSum + scoreCalList[curIdx][0], curIdx + 1) # curIdx에서 갱신 (고름)
 
 
 tc = int(input())
