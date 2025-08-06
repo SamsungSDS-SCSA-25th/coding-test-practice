7a2093797966076002f6bb97cbae3ea86d7134f0
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-06 10:59:15 +0900

    Refactor: 최소생산비용 (백트래킹) -> set을 인자를 넘겨받으면 재귀중에 시간복잡도가 증가함 -> visited를 넣고 원상복구 하는식으로 풀이

--- SWEA/2주차/250806(백트래킹 실전)/최소생산비용/최소생산비용.py
+++ SWEA/2주차/250806(백트래킹 실전)/최소생산비용/최소생산비용.py
@@ -1,8 +1,9 @@
 # 최소생산비용, N이 미지수, 가능성 여러가지 -> 백트래킹
 # N종의 제품을 N곳의 공장에서 한 곳당 한가지씩 생산
+### -> visited를 사용하자...
 
-def dfs(curRow, curSum, usedCols):
-    global N, matrix, minSum
+def dfs(curRow, curSum):
+    global N, matrix, minSum, visited
     #1 가지치기
     if minSum <= curSum:
         return
@@ -15,15 +16,18 @@ def dfs(curRow, curSum, usedCols):
 
     #3 하부재귀
     for curCol in range(N):
-        if curCol not in usedCols:
-            dfs(curRow+1, curSum+matrix[curRow][curCol], usedCols | {curCol})
+        if not visited[curCol]:
+            visited[curCol] = True
+            dfs(curRow+1, curSum+matrix[curRow][curCol])
+            visited[curCol] = False
 
 
 tc = int(input())
 for i in range(1, tc+1):
     N = int(input())
     matrix = [ list(map(int, input().split())) for _ in range(N) ]
+    visited = [False] * N
 
     minSum = 100*10
-    dfs(0, 0, set())
+    dfs(0, 0)
     print(f'#{i} {minSum}')
