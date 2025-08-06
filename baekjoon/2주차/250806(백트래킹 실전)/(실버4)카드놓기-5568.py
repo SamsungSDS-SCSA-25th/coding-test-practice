6fa4ce4c6ac12d7bcb3d8bef7715c8c60c18645f
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-06 16:30:18 +0900

    Refactor: 카드놓기 (백트래킹) -> 아래에서 추가로 set을 최대한 활용

--- baekjoon/2주차/250806(백트래킹 실전)/(실버4)카드놓기-5568.py
+++ baekjoon/2주차/250806(백트래킹 실전)/(실버4)카드놓기-5568.py
@@ -1,17 +1,16 @@
-# k가 미정, k개의 순열 -> 순열중에 겹치는 것? -> set에 넣고 돌리는 방법
+# k가 미정, k개의 순열 -> 순열중에 겹치는 것? -> set에 넣고 돌리는 방법 -> (D) 중복제거를 최대한 활용하기
 # 백트래킹
 ## 순열의 visited는 항상 idx순으로 넣기
 
 def dfs(curDepth, answer):
-    global n, k, cards, visited, cnt, repeatSet
+    global n, k, cards, visited, cnt, answerSet
     # 가지치기 불가
     # 종료조건
     if curDepth == k:
-        tempNum = int("".join(list(map(str, answer))))
-        if tempNum not in repeatSet:
+        # tempNum = int("".join(list(map(str, answer))))
+        # if tempNum not in repeatSet:
             # print(tempNum)
-            repeatSet.add(tempNum)
-            cnt += 1
+        answerSet.add("".join(list(map(str, answer)))) # (D) set은 중복되는 것 없앰. 따로 중복체크할 필요 없음
         return
     # 하부재귀
     for idx in range(n):
@@ -25,10 +24,11 @@ n = int(input())
 k = int(input())
 cards = [ int(input()) for _ in range(n) ]
 
-# (D) idx 순으로 검사하면, 중복되는 숫자 괜찮다
+# (D) idx 순으로 방문행렬 검사하면, 중복되는 숫자 괜찮다
 visited = [False] * n
 
-repeatSet = set()
-cnt = 0
+answerSet = set()
 dfs(0, [])
+# print(answerSet)
+cnt = len(answerSet)
 print(cnt)
