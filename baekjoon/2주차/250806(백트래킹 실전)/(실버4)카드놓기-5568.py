a2480a35d249fc94e53cd330a0dccd2772ef62ba
Eric SHIM <hyunwoo8504@gmail.com>
2025-08-06 16:20:32 +0900

    Refactor: 카드놓기 (백트래킹) -> 순열은 방문행렬을 이용, idx순으로 방문행렬 쌓기(같은수 등장해도 괜찮음)

--- baekjoon/2주차/250806(백트래킹 실전)/(실버4)카드놓기-5568.py
+++ baekjoon/2주차/250806(백트래킹 실전)/(실버4)카드놓기-5568.py
@@ -1,6 +1,6 @@
 # k가 미정, k개의 순열 -> 순열중에 겹치는 것? -> set에 넣고 돌리는 방법
 # 백트래킹
-## visited 구현 다르게 하는 방법 알아두기 (아래)
+## 순열의 visited는 항상 idx순으로 넣기
 
 def dfs(curDepth, answer):
     global n, k, cards, visited, cnt, repeatSet
@@ -14,23 +14,21 @@ def dfs(curDepth, answer):
             cnt += 1
         return
     # 하부재귀
-    for card in cards:
-        if visited[card] != 0:
-            visited[card] -= 1
-            dfs(curDepth + 1, answer + [card])
-            visited[card] += 1
+    for idx in range(n):
+        if not visited[idx]:
+            visited[idx] = True
+            dfs(curDepth + 1, answer + [cards[idx]])
+            visited[idx] = False
 
 
 n = int(input())
 k = int(input())
 cards = [ int(input()) for _ in range(n) ]
 
-repeatSet = set()
-# card 번호가 겹칠 수 있음 -> visited 다르게 구현
-visited = [0] * (99+1)
-for card in cards:
-    visited[card] += 1
+# (D) idx 순으로 검사하면, 중복되는 숫자 괜찮다
+visited = [False] * n
 
+repeatSet = set()
 cnt = 0
 dfs(0, [])
 print(cnt)
