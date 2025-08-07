52067ea521f6f5e2353a3e93d701e9d2b6445ea8
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-08-07 23:45:31 +0900

    Feat: 로봇청소기 (구현,시뮬레이션) -> 좌표잘보기,반시계방향 회전(%),for-else문

--- 삼성기출/(골드5)로봇청소기-14503.py
+++ 삼성기출/(골드5)로봇청소기-14503.py
@@ -1,62 +1,50 @@
 ## validate
-# 0은 청소되지 않은 칸 -> visited로 처리
+# 0은 청소되지 않은 칸 -> 청소하면 2로 처리할 예정
 # 1은 벽 -> matrix에서 1인 칸 못가도록 처리
-# 4칸을 바라보고 행동 -> BFS사용
+# BFS 아님 그냥 시뮬레이션...
 
-from collections import deque
-DIRECTIONS = [(0,1), (1,0), (0,-1), (-1,0)]
-def bfs(startCol, startRow, startDir):
-    global visited, answer
-    q = deque([(startCol, startRow, startDir)])
-    visited[startRow][startCol] = True
+def clean(startCol, startRow, startDir):
+    global answer
+    # 처음 청소 시작
+    matrix[startRow][startCol] = 2
+    answer += 1
 
-    while q:
-        curCol, curRow, curDir = q.popleft()
-        print(f'{curCol=}, {curRow=}, {curDir=}')
-        #1 4칸 확인
-        for dx, dy in DIRECTIONS:
+    endFlag = False
+    idx = 0
+    DIRECTIONS = [(0, -1), (1, 0), (0, 1), (-1, 0)] # (D) 북동남서 -> 좌표때문에 디버깅 했다.. 문제를 잘읽자
+    while True:
+
+        if endFlag:
+            break
+
+        if not idx:
+            curCol, curRow, curDir = startCol, startRow, startDir
+        elif idx:
+            curCol, curRow, curDir = nxtCol, nxtRow, nxtDir
+
+        # 반시계 방향으로 4칸 확인 후 청소하러 들어감
+        for dirIdx in range(1, 5):
+            nxtDir = (curDir - dirIdx) % 4
+            dx, dy = DIRECTIONS[nxtDir]
             nxtCol, nxtRow = curCol + dx, curRow + dy
-            #2-1 청소 진행하면됨 -> 무조건 청소할 곳이 있음
-            if 0 <= nxtCol < M and 0 <= nxtRow < N and not visited[nxtRow][nxtCol] and visited[nxtRow][nxtCol] == 0:
-                # 반복문
-                idx = 0
-                nnxtDir = (curDir - 1) % 4
-                nnxtCol, nnxtRow = curCol + DIRECTIONS[nnxtDir][0], curRow + DIRECTIONS[nnxtDir][1]
-                while True:
-                    # 반시계방향 회전 -> curDir에서 1씩 빼고 % 4
-                    if idx:
-                        nnxtDir = (nnxtDir-1) % 4
-                        nnxtCol, nnxtRow = nnxtCol + DIRECTIONS[nnxtDir][0], nnxtRow + DIRECTIONS[nnxtDir][1]
-                    print(f'{nnxtDir=}')
-                    print(f'{q=}')
-                    # 바라보는 방향이 빈칸인지 확인 -> 빈칸이면 반복문 탈출
-                    if matrix[nnxtRow][nnxtCol] == 0 and not visited[nnxtRow][nnxtCol]:
-                        # q에 넣고 , cnt 올리고, 방문처리
-                        q.append((nnxtCol, nnxtRow, nnxtDir))
-                        print(f'{q=}')
-                        visited[nnxtRow][nnxtCol] = True
-                        answer += 1
-                        break
+            # 반시계 방향으로 돌다가 청소하지 않은 곳으로 전진
+            if matrix[nxtRow][nxtCol] == 0: # 1로 둘러쌓여 있어 범위는 안봐도 됨
+                matrix[nxtRow][nxtCol] = 2
+                answer += 1
+                break
 
-                    idx += 1
+        ## 위 4방향 확인 후 -> 청소진행가능한 곳이 없었음 (for-else문)
+        # curDir에서 2 더하고 % 4 방향으로 후진 반복
         else:
-            ## 위 4방향 확인 후 -> 청소진행가능한 곳이 없었음
-            #2-2 4방향 모두 청소됨
-            # curDir에서 2 더하고 % 4 방향으로 후진 반복
-            nxtDir = (curDir + 2) % 4
-            nxtCol, nxtRow = curCol + DIRECTIONS[nxtDir][0], curRow + DIRECTIONS[nxtDir][1]
-            idx = 0
-            while True:
-                print(f'{answer=}')
-                # 후진한 곳이 갈 수 있는 곳이라면
-                if 0<=nxtCol<M and 0<=nxtRow<N and matrix[nxtRow][nxtCol] == 0:
-                    if idx:
-                        nxtCol, nxtRow = nxtCol + DIRECTIONS[nxtDir][0], nxtRow + DIRECTIONS[nxtDir][1]
-                # 후진 하는 방향이 벽이면 그대로 멈춘다.
-                if matrix[nxtRow][nxtCol] == 1:
-                    break
+            backDir = (curDir + 2) % 4
+            nxtCol, nxtRow = curCol + DIRECTIONS[backDir][0], curRow + DIRECTIONS[backDir][1]
 
-                idx += 1
+            # 후진 하는 방향이 벽이면 그대로 멈춘다.
+            if matrix[nxtRow][nxtCol] == 1:
+                endFlag = True
+                break
+
+        idx += 1
 
 
 # 0 북 / 1 동 / 2 남 / 3 서
@@ -64,7 +52,7 @@ N, M = map(int, input().split())
 startRow, startCol, startDir = map(int, input().split())
 matrix = [ list(map(int, input().split())) for _ in range(N) ]
 
-visited = [ [False]*M for _ in range(N) ]
-answer = 1 # 처음에 빈칸은 청소하지 않은 상태
-bfs(startCol, startRow, startDir)
+answer = 0
+clean(startCol, startRow, startDir)
+# print(matrix)
 print(answer)
